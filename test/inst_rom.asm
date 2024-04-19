8e58eefd32fd31f78f5f018e051bbd72a46bf867
CedricX <ct.xu@outlook.com>
2024-04-19 11:42:16 +0800

    add load relate test

--- test/inst_rom.asm
+++ test/inst_rom.asm
@@ -5,44 +5,49 @@ inst_rom.om：     文件格式 elf32-tradbigmips
 Disassembly of section .text:
 
 00000000 <_start>:
-   0:	34011234 	li	at,0x1234
-   4:	ac010000 	sw	at,0(zero)
-   8:	34021234 	li	v0,0x1234
-   c:	34010000 	li	at,0x0
-  10:	8c010000 	lw	at,0(zero)
-  14:	10220003 	beq	at,v0,24 <Label>
-  18:	00000000 	nop
-  1c:	34014567 	li	at,0x4567
-  20:	00000000 	nop
+   0:	34080001 	li	t0,0x1
+   4:	34090001 	li	t1,0x1
+   8:	34110004 	li	s1,0x4
+   c:	340c0100 	li	t4,0x100
+  10:	3c048040 	lui	a0,0x8040
+  14:	008c6821 	addu	t5,a0,t4
 
-00000024 <Label>:
-  24:	340189ab 	li	at,0x89ab
-  28:	00000000 	nop
+00000018 <loop>:
+  18:	01095021 	addu	t2,t0,t1
+  1c:	35280000 	ori	t0,t1,0x0
+  20:	35490000 	ori	t1,t2,0x0
+  24:	ac890000 	sw	t1,0(a0)
+  28:	8c8b0000 	lw	t3,0(a0)
+  2c:	152b0004 	bne	t1,t3,40 <end>
+  30:	34000000 	li	zero,0x0
+  34:	00912021 	addu	a0,a0,s1
+  38:	148dfff7 	bne	a0,t5,18 <loop>
+  3c:	34000000 	li	zero,0x0
 
-0000002c <_loop>:
-  2c:	0800000b 	j	2c <_loop>
-  30:	00000000 	nop
+00000040 <end>:
+  40:	1620ffff 	bnez	s1,40 <end>
+  44:	34000000 	li	zero,0x0
 	...
 
 Disassembly of section .reginfo:
 
-00000040 <.reginfo>:
-  40:	00000006 	srlv	zero,zero,zero
+00000050 <.reginfo>:
+  50:	00023f10 	0x23f10
 	...
 
 Disassembly of section .MIPS.abiflags:
 
-00000058 <_ram_end-0x18>:
-  58:	00002001 	movf	a0,zero,$fcc0
-  5c:	01010001 	movt	zero,t0,$fcc0
+00000068 <_ram_end-0x18>:
+  68:	00002001 	movf	a0,zero,$fcc0
+  6c:	01010001 	movt	zero,t0,$fcc0
 	...
-  68:	00000001 	movf	zero,zero,$fcc0
-  6c:	00000000 	nop
+  78:	00000001 	movf	zero,zero,$fcc0
+  7c:	00000000 	nop
 
 Disassembly of section .gnu.attributes:
 
 00000000 <.gnu.attributes>:
    0:	41000000 	0x41000000
-   4:	0f676e75 	jal	d9db9d4 <_ram_end+0xd9db964>
+   4:	0f676e75 	jal	d9db9d4 <_ram_end+0xd9db954>
    8:	00010000 	sll	zero,at,0x0
    c:	00070401 	0x70401
